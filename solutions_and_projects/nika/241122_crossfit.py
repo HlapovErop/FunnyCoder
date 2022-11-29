83417eea96316c92eb35dc2b53bdfeb24bc27708
nika953 <nika.kosheleva.06@mail.ru>
2022-11-29 19:35:18 +0300

    что-то сделала

--- solutions_and_projects/nika/241122_crossfit.py
+++ solutions_and_projects/nika/241122_crossfit.py
@@ -1,3 +1,4 @@
+from random import randint
 from statistics import mean
 
 array_for_test = [1, 45, 545, 65, 6]
@@ -11,6 +12,7 @@ array8_for_test = [-1, 45, -545, 65, 6]
 array10_for_test = [-1, 45, -545, 65, 6]
 array131_for_test = [-1, -45, -545, 65, -6]
 array132_for_test = [1, 45, -545, -65, 6]
+array19_for_test = [0, 8, 5, 34, 6, 5]
 
 
 def task1(array):
@@ -90,11 +92,49 @@ def task13(array):
     return False
 
 
-def task14(array):
-    max_value = 0
-    count = 0
-    for i in range(len(array) - 1):
-        if array[i]
+class Task16:
+    def __init__(self, idk, idk2, idk3):
+        self.idk = idk
+        self.idk2 = idk2
+        self.__idk3 = idk3
+
+    def get_idk3(self):
+        return self.__idk3
+
+    def set_idk3(self, value):
+        self.__idk3 = value
+
+ex1 = Task16(1,2,3)
+# print(ex1.get_idk3())
+# ex1.set_idk3(input())
+# print(ex1.get_idk3())
+
+def task19(array):
+    min_value = array[0]
+    for el in array:
+        if el < min_value:
+            min_value = el
+    value = min_value + 5
+    i = 0
+    for el in array:
+        if el == value:
+            i += 1
+    return i
+
+task20 = lambda: [randint(20, 200) for i in range(0, 10)]
+
+task21 = lambda n: [task20() for i in range(n)]
+
+def task22():
+    class RandomNumber():
+        def init(self):
+            self.value = randint(0, 200)
+    array = [RandomNumber() for i in range(10)]
+    for el in array:
+        if el.value > 100:
+            print(f"элемент-{el.value}, ", end="")
+    print()
+
 
 print(task1(array_for_test))
 print(task2(array2_for_test))
@@ -108,4 +148,6 @@ print(task9(6))
 print(task10(array10_for_test))
 print(task13(array131_for_test))
 print(task13(array132_for_test))
-
+print(task19(array19_for_test))
+print(task21(5))
+task22()
